60e59588f43f28c9d340af029a4098220062c4d1
zhengqiang130 <qzheng@suse.com>
2019-03-04 15:15:39 +0800

    iii

--- muduo/base/Thread.cpp
+++ muduo/base/Thread.cpp
@@ -1,3 +1,155 @@
 #include "muduo/base/Thread.h"
 #include "muduo/base/CurrentThread.h"
 #include "muduo/base/Exception.h"
+#include "muduo/base/Logging.h"
+
+#include "type_traits"
+
+#include "errno.h"
+#include <linux/unistd.h>
+#include <stdio.h>
+#include <sys/prctl.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+namespace muduo {
+namespace detail {
+
+pid_t gettid() { return static_cast<pid_t>(::syscall(SYS_gettid)); }
+
+void afterFork() {
+  muduo::CurrentThread::t_cachedTid = 0;
+  muduo::CurrentThread::t_threadName = "main";
+  CurrentThread::tid();
+}
+
+class ThreadNameInitializer {
+public:
+  ThreadNameInitializer() {
+    muduo::CurrentThread::t_threadName = "main";
+    CurrentThread::tid();
+    pthread_atfork(NULL, NULL, &afterFork);
+  }
+};
+
+ThreadNameInitializer init;
+
+struct ThreadData {
+  typedef muduo::Thread::ThreadFunc ThreadFunc;
+  ThreadFunc func_;
+  string name_;
+  pid_t *tid_;
+  CountDownLatch *latch_;
+
+  ThreadData(ThreadFunc func, const string &name, pid_t *tid,
+             CountDownLatch *latch)
+      : func_(std::move(func)), name_(name), tid_(tid), latch_(latch) {}
+
+  void runInThread() {
+    *tid_ = muduo::CurrentThread::tid();
+    tid_ = NULL;
+    latch_->countDown();
+    latch_ = NULL;
+
+    muduo::CurrentThread::t_threadName =
+        name_.empty() ? "muduoThread" : name_.c_str();
+    prctl(PR_SET_NAME, muduo::CurrentThread::t_threadName);
+
+    try {
+      func_();
+      muduo::CurrentThread::t_threadName = "finished";
+    } catch (const Exception &ex) {
+      muduo::CurrentThread::t_threadName = "crashed";
+      fprintf(stderr, "exception caught in thread %s\n", name_.c_str());
+      fprintf(stderr, "reason: %s\n", ex.what());
+      fprintf(stderr, "stack trace: %s\n", ex.stackTrace());
+      abort();
+
+    } catch (const std::exception &ex) {
+      muduo::CurrentThread::t_threadName = "crashed";
+      fprintf(stderr, "exception caught in thread %s\n", name_.c_str());
+      fprintf(stderr, "reason: %s\n", ex.what());
+      abort();
+    } catch (...) {
+      muduo::CurrentThread::t_threadName = "crashed";
+      fprintf(stderr, "exception caught in thread %s\n", name_.c_str());
+      throw;
+    }
+  }
+};
+
+void *startThread(void *obj) {
+  ThreadData *data = static_cast<ThreadData *>(obj);
+  data->runInThread();
+  delete data;
+  return NULL;
+}
+
+} // namespace detail
+
+void CurrentThread::cacheTid() {
+  if (t_cachedTid == 0) {
+    t_cachedTid = detail::gettid();
+    t_tidStringLength =
+        snprintf(t_tidString, sizeof t_tidString, "%5d", t_cachedTid);
+  }
+}
+
+bool CurrentThread::isMainThread() { return t_cachedTid == ::getpid(); }
+
+void CurrentThread::sleepUsec(int64_t usec) {
+  struct timespec ts = {0, 0};
+  ts.tv_sec = static_cast<time_t>(usec / Timestamp::KMicroSecondsPerSecond);
+  ts.tv_nsec =
+      static_cast<long>(usec % Timestamp::KMicroSecondsPerSecond * 1000);
+  ::nanosleep(&ts, NULL);
+}
+
+AtomicInt32 Thread::numCreated_;
+
+Thread::Thread(ThreadFunc func, const string &n)
+    : started_(false), joined_(false), pthreadId_(0), tid_(0),
+      func_(std::move(func)), name_(n), latch_(1) {
+  setDefaultName();
+}
+
+Thread::~Thread() {
+  if (started_ && !joined_) {
+    pthread_detach(pthreadId_);
+  }
+}
+
+void Thread::setDefaultName() {
+  int num = numCreated_.incrementAndGet();
+  if (name_.empty()) {
+    char buf[32];
+    snprintf(buf, sizeof buf, "Thread%d", num);
+    name_ = buf;
+  }
+}
+
+void Thread::start() {
+  assert(!started_);
+  started_ = true;
+  detail::ThreadData *data =
+      new detail::ThreadData(func_, name_, &tid_, &latch_);
+
+  if (pthread_create(&pthreadId_, NULL, detail::startThread, data)) {
+    started_ = false;
+    delete data;
+    LOG_SYSFATAL << "Failed in pthread create";
+  } else {
+    latch_.wait();
+    assert(tid_ > 0);
+  }
+}
+
+int Thread::join() {
+  assert(started_);
+  assert(!joined_);
+  joined_ = true;
+  return pthread_join(pthreadId_, NULL);
+}
+
+} // namespace muduo
--- muduo/base/Thread.h
+++ muduo/base/Thread.h
@@ -3,30 +3,36 @@
 
 #include <muduo/base/Atomic.h>
 #include <muduo/base/CountDownLatch.h>
-#include <muduo/base/noncopyable.h>
+#include <muduo/base/Types.h>
 
 #include <functional>
-#include <string>
+#include <memory>
+#include <pthread.h>
 
 namespace muduo {
 
 class Thread : public noncopyable {
 public:
-  using ThreadFunction = std::function<void(void)>;
-  explicit Thread(ThreadFunction, const std::string name = std::string());
+  using ThreadFunc = std::function<void(void)>;
+  explicit Thread(ThreadFunc, const std::string &name = std::string());
   ~Thread();
 
   void start();
   int join();
   bool started() { return started_; }
   pid_t tid() const { return tid_; }
+  const string &name() { return name_; }
+
+  static int numCreated() { return numCreated_.get(); }
 
 private:
+  void setDefaultName();
+
   bool started_;
   bool joined_;
-  pthread_t pthreadId;
+  pthread_t pthreadId_;
   pid_t tid_;
-  ThreadFunction func_;
+  ThreadFunc func_;
   std::string name_;
   CountDownLatch latch_;
   static AtomicInt32 numCreated_;

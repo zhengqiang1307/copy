3c654af26d31f4ede822879c9aa165c31429e917
zhengqiang130 <qzheng@suse.com>
2019-05-10 10:10:31 +0800

    iiii

--- muduo/base/LogStream.cpp
+++ muduo/base/LogStream.cpp
@@ -45,51 +45,34 @@ template <typename T> size_t convert(char buf[], T value) {
 
   return p - buf;
 }
+
 size_t convertHex(char buf[], uintptr_t value) {
   uintptr_t i = value;
   char *p = buf;
-
   do {
-    int lsd = static_cast<int>(i % 16);
+    int lsd = i % 16;
     i /= 16;
     *p++ = digitsHex[lsd];
   } while (i != 0);
   *p = '\0';
   std::reverse(buf, p);
-
   return p - buf;
 }
 
-template class FixedBuffer<kSmallBuffer>;
-template class FixedBuffer<kLargeBuffer>;
+template class FixedBuffer<KSmallBuffer>;
+template class FixedBuffer<KLargeBuffer>;
 
 } // namespace detail
 } // namespace muduo
-
-template <int SIZE> void FixedBuffer<SIZE>::cookieStart() {}
-
-template <int SIZE> void FixedBuffer<SIZE>::cookieEnd() {}
 
 template <int SIZE> const char *FixedBuffer<SIZE>::debugString() {
   *cur_ = '\0';
   return data_;
 }
 
-void LogStream::staticCheck() {
-  static_assert(kMaxNumericSize - 10 > std::numeric_limits<double>::digits10,
-                "kMaxNumericSize is large enough");
-  static_assert(kMaxNumericSize - 10 >
-                    std::numeric_limits<long double>::digits10,
-                "kMaxNumericSize is large enough");
-  static_assert(kMaxNumericSize - 10 > std::numeric_limits<long>::digits10,
-                "kMaxNumericSize is large enough");
-  static_assert(kMaxNumericSize - 10 > std::numeric_limits<long long>::digits10,
-                "kMaxNumericSize is large enough");
-}
-
 template <typename T> void LogStream::formatInteger(T v) {
   if (buffer_.avail() >= kMaxNumericSize) {
-    int len = static_cast<int>(convert(buffer_.current(), v));
+    size_t len = convert(buffer_.current(), v);
     buffer_.add(len);
   }
 }
@@ -99,55 +82,56 @@ LogStream &LogStream::operator<<(short v) {
   return *this;
 }
 
-LogStream &LogStream::operator<<(unsigned short v) {
+LogStream::self &LogStream::operator<<(unsigned short v) {
   *this << static_cast<unsigned int>(v);
   return *this;
 }
 
-LogStream &LogStream::operator<<(int v) {
+LogStream::self &LogStream::operator<<(int v) {
   formatInteger(v);
   return *this;
 }
 
-LogStream &LogStream::operator<<(unsigned int v) {
+LogStream::self &LogStream::operator<<(unsigned int v) {
   formatInteger(v);
   return *this;
 }
 
-LogStream &LogStream::operator<<(long v) {
+LogStream::self &LogStream::operator<<(long v) {
   formatInteger(v);
   return *this;
 }
 
-LogStream &LogStream::operator<<(unsigned long v) {
+LogStream::self &LogStream::operator<<(unsigned long v) {
   formatInteger(v);
   return *this;
 }
 
-LogStream &LogStream::operator<<(long long v) {
+LogStream::self &LogStream::operator<<(long long v) {
   formatInteger(v);
   return *this;
 }
 
-LogStream &LogStream::operator<<(unsigned long long v) {
+LogStream::self &LogStream::operator<<(unsigned long long v) {
   formatInteger(v);
   return *this;
 }
 
-LogStream &LogStream::operator<<(const void *p) {
-  uintptr_t v = reinterpret_cast<uintptr_t>(p);
+LogStream::self &LogStream::operator<<(const void *p) {
+  uintptr_t i = reinterpret_cast<uintptr_t>(p);
+
   if (buffer_.avail() >= kMaxNumericSize) {
-    char *buf = buffer_.current();
-    buf[0] = '0';
-    buf[1] = 'x';
-    size_t len = convertHex(buf + 2, v);
+    char *cur = buffer_.current();
+    *cur++ = '0';
+    *cur++ = 'x';
+
+    size_t len = convertHex(cur, i);
     buffer_.add(len + 2);
   }
   return *this;
 }
 
-// FIXME: replace this with Grisu3 by Florian Loitsch.
-LogStream &LogStream::operator<<(double v) {
+LogStream::self &LogStream::operator<<(double v) {
   if (buffer_.avail() >= kMaxNumericSize) {
     int len = snprintf(buffer_.current(), kMaxNumericSize, "%.12g", v);
     buffer_.add(len);
@@ -157,24 +141,9 @@ LogStream &LogStream::operator<<(double v) {
 
 template <typename T> Fmt::Fmt(const char *fmt, T val) {
   static_assert(std::is_arithmetic<T>::value == true,
-                "Must be arithmetic type");
-
+                "must be arithmetic type");
   length_ = snprintf(buf_, sizeof buf_, fmt, val);
   assert(static_cast<size_t>(length_) < sizeof buf_);
 }
 
-// Explicit instantiations
 
-template Fmt::Fmt(const char *fmt, char);
-
-template Fmt::Fmt(const char *fmt, short);
-template Fmt::Fmt(const char *fmt, unsigned short);
-template Fmt::Fmt(const char *fmt, int);
-template Fmt::Fmt(const char *fmt, unsigned int);
-template Fmt::Fmt(const char *fmt, long);
-template Fmt::Fmt(const char *fmt, unsigned long);
-template Fmt::Fmt(const char *fmt, long long);
-template Fmt::Fmt(const char *fmt, unsigned long long);
-
-template Fmt::Fmt(const char *fmt, float);
-template Fmt::Fmt(const char *fmt, double);
